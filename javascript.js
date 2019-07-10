77608a3f2836edd9032ec46ec716620ef1b16a05
Nithin.S <47477982+NithinSGowda@users.noreply.github.com>
2019-07-11 00:10:44 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("btn")
+
+button.addEventListener('click',()=>{
+    document.getElementById("disp").innerHTML="Developed by Nithin S. © All rights reserved"
+})
